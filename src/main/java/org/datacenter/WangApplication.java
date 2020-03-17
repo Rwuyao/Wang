286ed2632e5e84a54f45590871596504481bc946
Greate Xiong <282747051@qq.com>
2020-03-17 19:51:00 +0800

    init project

---
+++ src/main/java/org/datacenter/WangApplication.java
@@ -0,0 +1,13 @@
+package org.datacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WangApplication.class, args);
+	}
+
+}
