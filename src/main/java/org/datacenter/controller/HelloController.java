6efc92b1c7a285dead225e797ae4b260aacdf677
Greate Xiong <282747051@qq.com>
2020-03-17 20:24:44 +0800

    init project

---
+++ src/main/java/org/datacenter/controller/HelloController.java
@@ -0,0 +1,14 @@
+package org.datacenter.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/Hello")
+	public String welcome() {
+		return "HelloGit";
+	}
+}
+	
---
+++ src/test/java/org/datacenter/WangApplicationTests.java
@@ -0,0 +1,13 @@
+package org.datacenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
